295c25522fe1648601264c070efde38c52428cd9
Khanh <khanhngocdam>
2023-11-26 23:23:57 +0700

    Update test

--- AMIS-PROJECT/src/test/CalculateShippingFeeTest.java
+++ AMIS-PROJECT/src/test/CalculateShippingFeeTest.java
@@ -1,14 +1,14 @@
 package test;
 
 import controllers.PlaceOrderController;
+import models.Order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ValidateNameTest {
+public class CalculateShippingFeeTest {
     private PlaceOrderController placeOrderController;
     @BeforeEach
     void setUp() throws Exception {
@@ -17,17 +17,11 @@ public class ValidateNameTest {
 
     @ParameterizedTest
     @CsvSource({
-            "Đàm Ngọc Khánh, true",
-            "Đàm Ngọc Khánh 20205207, false",
-            "Đàm NgọcKhánh, true",
-            "Dam-Khanh, false",
-            "   , false",
+        
     })
 
-    public void test(String name, boolean expected) {
-        boolean isValid = placeOrderController.validateName(name);
-        assertEquals(expected, isValid);
+    public void test(Order order, double shippingFeeExpected) {
+        double shippingFee  = placeOrderController.calculateShippingFee(order);
+        assertEquals(shippingFeeExpected, shippingFee );
     }
-
-
 }
